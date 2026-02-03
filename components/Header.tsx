2d7ac27085757c014a3c4319543ea02f245f7e47
prasnjit-dey-ux <prasanjitdey8523@gmail.com>
2026-02-03 23:54:02 +0530

    updated header section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,47 @@
 export const Header = () => {
-    return (
-        <div className="font-inter text-base text-primary-text flex justify-between items-start pt-6">
-            <h1>Prasanjit Dey</h1>
-            <p>3:24PM</p>
-            <p>Get in touch</p>
-            <div className="flex flex-col gap-10 w-72">
-                <div className="flex justify-between items-start gap-10">
-                    <p>Design</p>
-                        <div className="flex flex-col gap-1">
-                            <p>Figma</p>
-                            <p>Prototyping</p>
-                            <p>Wireframing</p>
-                        </div>
-                </div>
-                <div className="flex justify-between items-start gap-10">
-                    <p>Development</p>
-                        <div className="flex flex-col gap-1">
-                            <p>HTML/CSS</p>
-                            <p>Typescript</p>
-                            <p>React JS</p>
-                            <p>React Motion</p>
-                            <p>Next JS</p>
-                        </div>
-                </div>
+  return (
+    <header className="font-inter text-base text-primary-text pt-6">
+      <div className="flex justify-between items-start">
 
+        {/* Column 1 */}
+        <h1 className="font-medium whitespace-nowrap">
+          Prasanjit Dey
+        </h1>
+
+        {/* Column 2 */}
+        <p className="whitespace-nowrap">
+          3:24 PM
+        </p>
+
+        {/* Column 3 */}
+        <p className="cursor-pointer text-primary whitespace-nowrap">
+          Get in touch
+        </p>
+
+        {/* Column 4 */}
+        <div className="flex flex-col gap-10 text-right">
+          <div className="flex justify-between w-72">
+            <p className="text-secondary-text">Design</p>
+            <div className="flex flex-col gap-1">
+              <p>Figma</p>
+              <p>Prototyping</p>
+              <p>Wireframing</p>
             </div>
+          </div>
+
+          <div className="flex justify-between gap-10">
+            <p className="text-secondary-text">Development</p>
+            <div className="flex flex-col gap-1">
+              <p>HTML/CSS</p>
+              <p>TypeScript</p>
+              <p>React</p>
+              <p>Motion</p>
+              <p>Next.js</p>
+            </div>
+          </div>
         </div>
-    )
-}
+
+      </div>
+    </header>
+  );
+};
